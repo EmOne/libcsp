97fa5f4446f2f6cca21860bcc020b8667341946b
Anol Paisal <anolpaisal@gmail.com>
2024-07-31 12:40:19 +0700

    Added YAMCS TC forwarding

--- examples/csp_client.c
+++ examples/csp_client.c
@@ -14,6 +14,9 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
@@ -34,7 +37,16 @@ static unsigned int successful_ping = 0;
 static unsigned int run_duration_in_sec = 3;
 
 static uint32_t can_bps = 1000000;
-static bool forwarding = false;
+static int forwarding = 0;
+struct sockaddr_can can_addr;
+struct ifreq ifr;
+struct can_frame frame;
+
+const char *ip = "127.0.0.1";
+const int port = 10025;
+int sockfd;
+struct sockaddr_in ccsds_addr;
+socklen_t addr_size;
 
 enum DeviceType {
 	DEVICE_UNKNOWN,
@@ -87,7 +99,7 @@ void print_help() {
 	}
 	if (CSP_HAVE_LIBZMQ) {
 		csp_print(" -z <zmq-device>  set ZeroMQ device\n");
-		csp_print(" -f <0|1>         forward CAN to ZMQ\n");
+		csp_print(" -f <0|1|2>       0:disable forwarding 1:CAN 2:CCSDS to ZMQ\n");
 	}
 	if (CSP_USE_RTABLE) {
 		csp_print(" -R <rtable>      set routing table\n");
@@ -155,13 +167,12 @@ int main(int argc, char * argv[]) {
 	int ret = EXIT_SUCCESS;
     int opt;
 
-	int s = 0, i; 
+	int s = 0, i;
 	int nbytes;
-	struct sockaddr_can addr;
-	struct ifreq ifr;
-	struct can_frame frame;
 	char frame_buff[32];
-	size_t alen;
+	char buffer[4096];
+	size_t alen = 0;
+	const uint32_t HEADER_SIZE = (csp_conf.version == 2) ? 6 : 4;
 
 	while ((opt = getopt_long(argc, argv, OPTION_c OPTION_z OPTION_f OPTION_R "k:a:C:tT:h", long_options, NULL)) != -1) {
         switch (opt) {
@@ -182,6 +193,7 @@ int main(int argc, char * argv[]) {
                 break;
             case 'f':
 				forwarding = atoi(optarg);
+				csp_print("Forwarding: %d\n",forwarding);
                 break;
 #if (CSP_USE_RTABLE)
             case 'R':
@@ -219,24 +231,50 @@ int main(int argc, char * argv[]) {
     }
 
 	csp_print("CAN Sockets Receive \n");
-	
-	if(forwarding) {
-		if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-			perror("CAN Socket");
-			exit(EXIT_FAILURE);
-		}
-	
-		strcpy(ifr.ifr_name, "can0" );
-		ioctl(s, SIOCGIFINDEX, &ifr);
 
-		memset(&addr, 0, sizeof(addr));
-		addr.can_family = AF_CAN;
-		addr.can_ifindex = ifr.ifr_ifindex;
+	switch(forwarding)
+	{
+		case 1: //CAN forwarding
+			if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+				perror("CAN Socket");
+				exit(EXIT_FAILURE);
+			}
 
-		if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-			perror("CAN Bind");
-			exit(EXIT_FAILURE);
-		}
+			strcpy(ifr.ifr_name, "can0" );
+			ioctl(s, SIOCGIFINDEX, &ifr);
+
+			memset(&can_addr, 0, sizeof(can_addr));
+			can_addr.can_family = AF_CAN;
+			can_addr.can_ifindex = ifr.ifr_ifindex;
+
+			if (bind(s, (struct sockaddr *)&can_addr, sizeof(can_addr)) < 0) {
+				perror("CAN Bind");
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 2:	//TC CCSDS forwarding
+			//#1 : socket
+			sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+			if(sockfd < 0)
+			{
+				perror("UDP socket");
+				exit(EXIT_FAILURE);
+			}
+			memset(&ccsds_addr, '\0', sizeof(ccsds_addr));
+			ccsds_addr.sin_family = AF_INET;
+			ccsds_addr.sin_port = htons(port);
+			ccsds_addr.sin_addr.s_addr = inet_addr(ip);
+			//#2 : binding the socket
+
+ 			if(bind(sockfd, (struct sockaddr*)&ccsds_addr, sizeof(ccsds_addr))<0)
+			{
+				perror("UDP binding");
+				exit(EXIT_FAILURE);
+			}
+
+			break;
+		default:
+			break;
 	}
 
     csp_print("Initialising CSP\n");
@@ -307,32 +345,50 @@ int main(int argc, char * argv[]) {
 			break;
 		}
 
-		if(forwarding) {
-			nbytes = read(s, &frame, sizeof(struct can_frame));
+		switch(forwarding)
+		{
+			case 1: //forwarding CAN raw
+				nbytes = read(s, &frame, sizeof(struct can_frame));
 
-			if (nbytes < 0) {
-				perror("CAN Read");
+				if (nbytes < 0) {
+					perror("CAN Read");
+					break;
+				}
+
+				csp_print("0x%03X [%d] ",frame.can_id  & 0xfff, frame.can_dlc);
+				alen = sprintf(frame_buff, "%03X#", frame.can_id & 0xfff);
+
+				for (i = 0; i < frame.can_dlc; i++)
+				{
+					csp_print("%02X ",frame.data[i]);
+					alen += sprintf(frame_buff + alen, "%02X", frame.data[i]);
+				}
 				break;
-			}
-			
-			csp_print("0x%03X [%d] ",frame.can_id  & 0xfff, frame.can_dlc);	
-			alen = sprintf(frame_buff, "%03X#", frame.can_id & 0xfff);	
-					
-			for (i = 0; i < frame.can_dlc; i++) 
-			{
-				csp_print("%02X ",frame.data[i]);
-				alen += sprintf(frame_buff + alen, "%02X", frame.data[i]);
-			}
-			
-		} else {
-			alen = sprintf(frame_buff, "%03X#", 0x201 & 0xfff);
-			alen += sprintf(frame_buff + alen, "%08X", count);
+			case 2: // forwarding the received TC from CCSDS
+				addr_size = sizeof(ccsds_addr);
+  				alen = recvfrom(sockfd, buffer, 4096, 0, (struct sockaddr*)&ccsds_addr, &addr_size);
+
+				csp_print("[+]CCSDS Data recv: %s (%X) len:%d\n", buffer, buffer, alen);
+
+				alen = sprintf(frame_buff, "%03X#",  (uint16_t)buffer[0] & 0xfff);
+
+				for (i = 3; i < 11; i++)
+				{
+					csp_print("%02X ",buffer[i]);
+					alen += sprintf(frame_buff + alen, "%02X", buffer[i]);
+				}
+				break;
+			default:
+				alen = sprintf(frame_buff, "%03X#", 0x201 & 0xfff);
+				alen += sprintf(frame_buff + alen, "%08X", count);
+				break;
 		}
+
 
 		csp_print("\n");
 
 		/* 2. Get packet buffer for message/data */
-		csp_packet_t * packet = csp_buffer_get(100);
+		csp_packet_t * packet = csp_buffer_get(alen + HEADER_SIZE);
 		if (packet == NULL) {
 			/* Could not get buffer element */
 			csp_print("Failed to get CSP buffer\n");
@@ -341,10 +397,6 @@ int main(int argc, char * argv[]) {
 		}
 
 		/* 3. Copy data to packet */
-		
-		// memcpy(packet->data, "EmOne->word: ", 12);
-		// memcpy(packet->data + 12, &count, 1);
-		// memset(packet->data + 13, 0, 1);
 		memcpy(packet->data, &frame_buff, alen);
 		memset(packet->data + alen, 0, 1);
 		csp_print("Sending MSG: %s\n", packet->data);
